dca075c6c6766550047b9e94557a3f7363eb196b
Leon Si <contact@leonzalion.com>
2022-01-07 20:08:57 -0500

    copy icons.css

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,4 +3,4 @@ import * as fs from 'fs';
 
 fs.rmSync('dist', { recursive: true, force: true });
 exec('tsc');
-exec('cp readme.md package.json dist');
+exec('cp icons.css readme.md package.json dist');
