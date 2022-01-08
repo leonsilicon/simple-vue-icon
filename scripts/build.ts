b26332cd12a8cfce0e3a9eedb4f056668c7acab2
Leon Si <contact@leonzalion.com>
2022-01-07 20:09:28 -0500

    fix build

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,4 +3,4 @@ import * as fs from 'fs';
 
 fs.rmSync('dist', { recursive: true, force: true });
 exec('tsc');
-exec('cp icons.css readme.md package.json dist');
+exec('cp src/icons.css readme.md package.json dist');
